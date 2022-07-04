130f69056fce28f62cdfc1b486da56f138b38a8e
Yahav Tzukerman <tzuki.yahav@gmail.com>
2022-07-04 20:24:38 +0300

    Initial Commit2

---
+++ src/main/java/com/example/datingWebsite/DatingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.datingWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingWebsiteApplication.class, args);
+	}
+
+}
